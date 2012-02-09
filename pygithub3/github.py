2489a209f831f26934535461ecdc7f87bbfd1185
David Medina <davidmedina9@gmail.com>
2012-02-09 02:06:44 +0100

    Joining components in a clean interface

--- pygithub3/github.py
+++ pygithub3/github.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
+from pygithub3.services.users import User
+
+
+class GitHub(object):
+    """ Main entrance """
+
+    def __init__(self, **config):
+        self._users = User(**config)
+
+    @property
+    def users(self):
+        return self._users
