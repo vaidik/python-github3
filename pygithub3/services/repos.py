99a601f0dbffac0b95e9997ee12221460a91e43f
David Medina <davidmedina9@gmail.com>
2012-02-19 19:46:57 +0100

    Add mimetype support to repos.commits service

--- pygithub3/services/repos.py
+++ pygithub3/services/repos.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from .base import Service
+from .base import Service, MimeTypeMixin
 
 
-class Commits(Service):
+class Commits(Service, MimeTypeMixin):
 
     """ TODO: Pagination structure differs from usual
     def list(self, user=None, repo=None, sha='', path=''):
@@ -20,22 +20,22 @@ class Commits(Service):
     def list_comments(self, sha=None, user=None, repo=None):
         request = self.make_request('repos.commits.list_comments',
             sha=sha, user=user, repo=repo)
-        return self._get_result(request)
+        return self._get_result(request, **self._get_mimetype_as_header())
 
     def create_comment(self, data, sha, user=None, repo=None):
         request = self.make_request('repos.commits.create_comment',
             sha=sha, user=user, repo=repo, body=data)
-        return self._post(request)
+        return self._post(request, **self._get_mimetype_as_header())
 
     def get_comment(self, cid, user=None, repo=None):
         request = self.make_request('repos.commits.get_comment',
             comment_id=cid, user=user, repo=repo)
-        return self._get(request)
+        return self._get(request, **self._get_mimetype_as_header())
 
     def update_comment(self, data, cid, user=None, repo=None):
         request = self.make_request('repos.commits.update_comment',
             comment_id=cid, user=user, repo=repo, body=data)
-        return self._patch(request)
+        return self._patch(request, **self._get_mimetype_as_header())
 
     def compare(self, base, head, user=None, repo=None):
         request = self.make_request('repos.commits.compare',
