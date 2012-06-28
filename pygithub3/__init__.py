6619b8c32183bea92152165285e96c2dc35efceb
David Medina <davidmedina9@gmail.com>
2012-06-28 23:27:43 +0200

    Bump 0.4.1
    Fix S3 upload

--- pygithub3/__init__.py
+++ pygithub3/__init__.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 __title__ = 'pygithub3'
-__version__ = '0.4'
+__version__ = '0.4.1'
 __author__ = 'David Medina'
 __email__ = 'davidmedina9@gmail.com'
 __license__ = 'ISC'
